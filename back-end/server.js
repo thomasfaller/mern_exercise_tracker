fb019463c82422743f4ae8ff67888826a15b5c67
Thomas <thomas@1457d.k.time4vps.cloud>
2019-07-01 11:58:33 +0300

    commiting back-end

---
+++ back-end/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const URI = process.env.ATLAS_URI;
+const OPTS = {
+  useNewUrlParser: true,
+  useCreateIndex: true
+};
+
+mongoose.connect(URI, OPTS, function (err) {
+  if (err) {
+    return console.error('failed: ', err);
+  }
+});
+
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+
+// const exercisesRouter = require('./routes/exercises');
+// const usersRouter = require('./routes/users');
+
+// app.use('/exercises', exercisesRouter);
+// app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
